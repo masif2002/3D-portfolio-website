22179abb36ad3fa49c9e46ff7c437ae745ab132b
masif2002 <masif2002@outlook.com>
2023-03-08 14:47:07 +0530

    Added Tech Section

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
+import { technologies } from '../constants'
+import {BallCanvas} from './canvas'
+import { SectionWrapper } from '../hoc'
+
 
 const Tech = () => {
   return (
-    <div>Tech</div>
+    <div className='flex flex-row flex-wrap justify-center gap-10'>
+      {technologies.map((tech) => (
+        
+        <div className="w-28 h-28" key={tech.name}>
+           <BallCanvas icon={tech.icon} />
+        </div>
+
+      ))}
+    </div>
   )
 }
 
-export default Tech
+export default SectionWrapper(Tech, "tech")
