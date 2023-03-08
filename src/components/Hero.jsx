944708ecabab19977d9e4bd25888e0aaa091ccc4
masif2002 <masif2002@outlook.com>
2023-03-08 05:56:47 +0530

    Done with Hero Section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,6 +4,7 @@ import { styles } from '../styles'
 import { ComputersCanvas } from './canvas'
 
 const Hero = () => {
+
   return (
     <section className="relative w-full h-screen mx-auto">
       <div className={`${styles.paddingX} absolute top-[120px] inset-0 max-w-7xl mx-auto flex flex-row items-start gap-5`}>
@@ -24,6 +25,24 @@ const Hero = () => {
       </div>
       
       <ComputersCanvas />
+
+      <div className="absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center">
+        <a href="#about">
+          <div className='w-[32px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2'>
+            <motion.dev 
+              animate = {{
+                y: [0, 24, 0]
+              }}
+              transition = {{
+                duration: 1.5,
+                repeat: Infinity,
+                repeatType: 'loop'
+              }}
+              className = "w-3 h-3 rounded-full bg-secondary mb-1"
+            />
+          </div>
+        </a>
+      </div>
     </section>
   )
 }
--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -5,8 +5,9 @@ import { OrbitControls, Preload, useGLTF } from '@react-three/drei'
 import CanvasLoader from '../Loader'
 import Loader from '../Loader'
 
-const Computers = () => {
+const Computers = ({ isMobile }) => {
   const computer = useGLTF('./desktop_pc/scene.gltf')
+
 
   return (
     <mesh>
@@ -24,7 +25,7 @@ const Computers = () => {
       />
       <primitive 
         object={computer.scene}
-        scale={0.75}
+        scale={isMobile ? 0.6 : 0.75}
         position={[0, -3.25, -1.5]}
         rotation={[-0.01, -0.2, -0.1]}
           />
@@ -33,6 +34,30 @@ const Computers = () => {
 }
 
 const ComputersCanvas = () => {
+
+  const [isMobile, setIsMobile] = useState(false)
+
+  useEffect(() => {
+    // Checking if it is a mobile view
+    const mediaQuery = window.matchMedia("(max-width: 500px)")
+    setIsMobile(mediaQuery.matches)
+
+    // If change in screen size, the state is updated
+    const handleMediaQueryChange = (event) => {
+      setIsMobile(event.matches)
+    }
+
+    // Incase of change in screen size, the function is called
+    mediaQuery.addEventListener("change", handleMediaQueryChange)
+
+  
+    return () => {
+      mediaQuery.removeEventListener("change", handleMediaQueryChange)
+    }
+
+  }, [])
+
+
   return (
     <Canvas
       frameloop='demand'
@@ -48,7 +73,7 @@ const ComputersCanvas = () => {
           maxPolarAngle={Math.PI / 2}
           minPolarAngle={Math.PI / 2}
         />
-        <Computers />
+        <Computers isMobile={isMobile} />
       </Suspense>
 
       <Preload all />
