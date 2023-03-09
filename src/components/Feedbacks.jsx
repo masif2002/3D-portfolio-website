b4d3dc6330c99b94778074e24ec77dcfe0f4ebb5
masif2002 <masif2002@outlook.com>
2023-03-09 06:48:01 +0530

    Added testimonials

--- src/components/Feedbacks.jsx
+++ src/components/Feedbacks.jsx
@@ -1,9 +1,62 @@
+import { motion } from 'framer-motion'
 import React from 'react'
+import { testimonials } from '../constants'
+import { SectionWrapper } from '../hoc'
+import { styles } from '../styles'
+import { fadeIn, textVariant } from '../utils/motion'
 
+
+const FeedbackCard = ({ index,testimonial, name, designation, company, image}) => {
+  return(
+    <motion.div variants={fadeIn("", "spring", index * 0.5, 0.75)} 
+    className="bg-black-200 p-10 rounded-3xl xs:w-[320px] w-full" >
+      <p className="text-white font-black text-[48px]">"</p>
+      <p>{testimonial}</p>
+
+      <div className="mt-7 flex justify-between items-center gap-1">
+        <div className="flex-1 flex flex-col">
+          <p className="text-white font-medium text-[16px]">
+            <span className="blute-text-gradient">@ </span>{name}
+          </p>
+          <p className="mt-1 text-secondary text-[12px]">
+            {designation} of {company}
+          </p>
+        </div>
+
+        <img src={image} alt={name}
+          className="w-10 h-10 rounded-full object-cover"
+        />
+      </div>
+    </motion.div>
+  )
+}
 const Feedbacks = () => {
   return (
-    <div>Feedbacks</div>
+    <div className='mt-12 bg-black-100 rounded-[20px]'>
+      <div className={`bg-tertiary rounded-2xl min-h-[300px] ${styles.padding}`}>
+
+        <motion.div variants={textVariant()}>
+          <p className={`${styles.sectionSubText}`}>
+            What Others Say
+          </p>
+          <h2 className={`${styles.sectionHeadText}`}>
+            Testimonials.
+          </h2>
+        </motion.div>
+      </div>
+
+      <div className={`${styles.paddingX} -mt-20 pb-14 flex flex-wrap gap-7`}>
+        {testimonials.map((testimonial, index) => (
+          <FeedbackCard 
+            key={testimonial.name}
+            index={index}
+            {...testimonial}
+          />
+        ))}
+
+      </div>
+    </div>
   )
 }
 
-export default Feedbacks
+export default SectionWrapper(Feedbacks, "")
