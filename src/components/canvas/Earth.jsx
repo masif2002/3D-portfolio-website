821ee5d83a88fe331792576f88c1899e88956d33
masif2002 <masif2002@outlook.com>
2023-03-11 05:38:28 +0530

    Added Earth Model

--- src/components/canvas/Earth.jsx
+++ src/components/canvas/Earth.jsx
@@ -1,9 +1,46 @@
-import React from 'react'
+import { OrbitControls, Preload, useGLTF } from '@react-three/drei'
+import { Canvas } from '@react-three/fiber'
+import React, { Suspense } from 'react'
+import CanvasLoader from '../Loader'
 
 const Earth = () => {
+  const earth = useGLTF('/planet/scene.gltf')
+
   return (
-    <div>Earth</div>
+    <primitive 
+      object={earth.scene}
+      scale={2.5}
+      position-y={0}
+      position-x={0}
+    />
   )
 }
 
-export default Earth
+const EarthCanvas = () => {
+
+  return (
+    <Canvas
+      shadows
+      frameloop="demand"
+      gl={{ preserveDrawingBuffer: true }}
+      camera={{
+        fov:45,
+        near:0.1,
+        far:200,
+        position: [-4, 3, 6]
+      }}
+    >
+      <Suspense fallback={<CanvasLoader />} >
+      <OrbitControls 
+        autoRotate
+        enableZoom={false}
+        maxPolarAngle={Math.PI/2}
+        minPolarAngle={Math.PI/2}
+      />
+        <Earth />
+      </Suspense>
+    </Canvas>
+  )
+}
+
+export default EarthCanvas
