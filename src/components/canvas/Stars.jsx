36a28b36e2ee60539c1c59498e2645c425a7c58b
masif2002 <masif2002@outlook.com>
2023-03-11 06:25:24 +0530

    Added Floating Stars

--- src/components/canvas/Stars.jsx
+++ src/components/canvas/Stars.jsx
@@ -1,9 +1,48 @@
-import React from 'react'
+import { PointMaterial, Points, Preload } from '@react-three/drei'
+import { Canvas, useFrame } from '@react-three/fiber'
+import * as random from "maath/random/dist/maath-random.esm";
+import React, { Suspense, useRef } from 'react'
 
-const Stars = () => {
+
+
+const Stars = (props) => {
+  const ref = useRef();
+
+  const sphere = random.inSphere(new Float32Array(5000), { radius: 1.5 })
+
+  useFrame((state, delta) => {
+    ref.current.rotation.x -= delta/10
+    ref.current.rotation.y -= delta/15
+  })
+
   return (
-    <div>Stars</div>
+    <group rotation={[0, 0, Math.PI/4]}>
+      <Points ref={ref} positions={sphere} stride={3} frustumCulled {...props} >
+        <PointMaterial 
+          transparent
+          color="#f272c8"
+          size={0.002}
+          sizeAttenuation={true}
+          depthWrite={false}
+        />
+      </Points>
+    </group>
   )
 }
 
-export default Stars
+const StarsCanvas = () => {
+  return (
+    <div className="w-full h-auto absolute inset-0 z-[-1]">
+      <Canvas camera={{ position: [0, 0, 1] }}>
+        <Suspense fallback={null}>
+          <Stars />
+        </Suspense>
+
+        <Preload all />
+        
+      </Canvas>
+    </div>
+  )
+}
+
+export default StarsCanvas
